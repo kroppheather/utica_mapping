827472cd52cc32320e75243977819f58b10834a9
Heather Kropp <kroppheather@gmail.com>
2021-09-14 20:05:36 -0400

    Update map_images.R

--- map_images.R
+++ map_images.R
@@ -1,91 +1,89 @@
 library(sf)
 library(raster)
 library(mapview)
-library(caret)
-library(nnet)
-library(keras)
 
+dirO <- "E:\\Google Drive\\GIS\\utica\\u_train"
+
+#### read in data and visualize ----
 trees <- read.csv("E:/Google Drive/GIS/utica/trees/trees_utica.csv")
 treeS <- st_as_sf(trees, coords=c("Longitude","Latitude"),
                   crs=4326)
 
 
-
 r50s <- raster("E:/Google Drive/GIS/utica/MyProject4/A550500171317_ref.tif")
 r50s@crs
-treeP <- st_transform(treeS, 3857)
-
 plot(r50s, col=gray(1:100/100))
-click()
-
 Ucenter <- extent(-8377200,-8373500,
                   5326000,5328800)
-u50s <- crop(r50s,Ucenter)
 
+Ucenter2 <- extent(-8382000,-8373500,
+                  5324000,5329000)
+
+u50a <- crop(r50s,Ucenter2)
+plot(u50a, col=gray(1:100/100))
 
 plot(u50s, col=gray(1:100/100))
 plot(treeP$geometry, col="forestgreen", add=TRUE)
 
-mapview(u50s, col=gray(1:100/100))#+
-  #mapview(treeCenter, color="forestgreen")
+mapview(u50a, col=gray(1:100/100))
 
 
 
-treeP <- drawFeatures()+mapview(u50s, col=gray(1:100/100))
-
-TreeT <- st_read("E:/Google Drive/GIS/utica/train/tree.shp")
 
 
-StreetT <- st_read("E:/Google Drive/GIS/utica/train/streets.shp")
 
-BuildingT <- st_read("E:/Google Drive/GIS/utica/train/building.shp")
 
-otherT <- st_read("E:/Google Drive/GIS/utica/train/other_fixed.shp")
+#### subset raster for training ----
+u50a
 
-plot(u50s, col=gray(1:100/100))
-plot(TreeT$geometry, col="red", add=TRUE)
-plot(StreetT$geometry, col="red", add=TRUE)
-plot(BuildingT$geometry, col="red", add=TRUE)
-plot(otherT$geometry, col="red", add=TRUE)
 
-#set up data frame that extracts all
-#1 = tree
-#2= street
-#3 = building
-#4 = other
+# subset size
+# 128 x 128 pixels
+#convert to matrix
 
-#sampleID, 1 is train, 2 is valid
 
-TreeT$classID <- rep(1, nrow(TreeT))
-TreeT$sampleID <- rep(1, nrow(TreeT))
-#sample half for valid
+
+test <- imgV[1:128,1:128]
+
+nSamp <- 20
+set.seed(42)
+samplesx <- sample(1:(u50a@ncols-257), nSamp)
 set.seed(12)
-TreeT$sampleID[sort(sample(seq(1,nrow(TreeT)),60))] <- 2
+samplesy <- sample(1:(u50a@nrows-257), nSamp)
 
-StreetT$classID <- rep(2, nrow(StreetT))
-StreetT$sampleID <- rep(1, nrow(StreetT))
-#sample half for valid
-set.seed(132)
-StreetT$sampleID[sort(sample(seq(1,nrow(StreetT)),60))] <- 2
+uSubs <- crop(u50a, extent(u50a, samplesy[1], 
+                           samplesy[1] +255, 
+                           samplesx[1], 
+                           samplesx[1]+255))
 
-BuildingT$classID <- rep(3, nrow(BuildingT))
-BuildingT$sampleID <- rep(1, nrow(BuildingT))
-#sample half for valid
-set.seed(132)
-BuildingT$sampleID[sort(sample(seq(1,nrow(BuildingT)),60))] <- 2
+uSubs
+plot(uSubs)
+writeRaster(uSubs, paste0(dirO,"test.tif"),
+            format="GTiff")
 
-otherT$classID <- rep(4, nrow(otherT))
-otherT$sampleID <- rep(1, nrow(otherT))
-#sample half for valid
-set.seed(33)
-otherT$sampleID[sort(sample(seq(1,nrow(otherT)),60))] <- 2
+which(is.na(getValues(uSubs)))
 
-allPTS <- rbind(TreeT[,2:4],StreetT[,2:4],BuildingT[,2:4],otherT[,2:4])
+for(i in 1:nSamp){
+  
+  
+  writeRaster(crop(u50a, extent(u50a, samplesy[i], 
+                                samplesy[i] +256, 
+                                samplesx[i], 
+                                samplesx[i]+256)), 
+              paste0(dirO, "\\train_",i,".tif"),
+             format="GTiff" )
+  
 
-trainPTs <- allPTS[allPTS$sampleID == 1,]
-vaildPTs <- allPTS[allPTS$sampleID == 2,]
+}
 
 
-#extract training
+plot(uSubs[[10]], col=gray(1:100/100))
 
-#extract data
+uSubs <- crop(u50a, extent(u50a, samplesy[1], 
+                                samplesy[1] +256, 
+                                samplesx[1], 
+                                samplesx[1]+256))
+
+
+test <- raster(paste0(dirO, "/train_",3,".tif"))
+plot(test)
