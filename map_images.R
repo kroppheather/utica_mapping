4b7c0e000403feb167e353655b651657ef3aac24
kroppheather <kroppheather@gmail.com>
2021-12-02 19:07:10 -0500

    img 28

--- map_images.R
+++ map_images.R
@@ -75,7 +75,7 @@ for(i in 1:20){
 #### Step 1: read in image   ##
 
 #give training image number
-trainNum <- 27
+trainNum <- 28
 
 imgN <- raster(paste0(dirO, "/50s_train/train_",trainNum,".tif"))
 plot(imgN, col=grey(1:100/100))
