c8b92624562c228712eedcd06e8304f2e5dd159c
kroppheather <kroppheather@gmail.com>
2021-12-28 14:10:00 -0500

    img 18

--- map_images.R
+++ map_images.R
@@ -162,7 +162,7 @@ writeRaster(paveMask, paste0(dirM,"/pavement/pavement_mask_",trainNum,".tif"),
 #### Step 1: read in image   ##
 
 #give valid image number
-validNum <- 17
+validNum <- 18
 
 imgN <- raster(paste0(dirO, "/50s_valid/valid_",validNum,".tif"))
 plot(imgN, col=grey(1:100/100))
