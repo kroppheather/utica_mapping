4874aeab5ca302cc120c85f128b3018b8a13067b
Heather Kropp <kroppheather@gmail.com>
2021-12-30 16:21:06 -0500

    finished visualization

--- prediction_50s.R
+++ prediction_50s.R
@@ -1,6 +1,6 @@
 library(raster)
 library(sf)
-
+library(mapview)
 
 dirP <- "E:/Google Drive/research/projects/utica/50s_predict_out"
 dirI <- "E:/Google Drive/research/projects/utica/predict50"
@@ -57,3 +57,25 @@ plot(treeMap)
 plot(origAll, col=gray(1:100/100))
 plot(buildMap)
 plot(paveMap)
+
+treeCol <- rgb(0.13,0.54,0.13,0.5)
+paveCol <- rgb(0.5,0.5,0.5,0.5)
+buildCol <- rgb(0.53,0.17,0.09,0.5)
+
+par(mfrow=c(1,2))
+par(mai=c(0,0,0,0))
+plot(treeMap, col=c(NA,treeCol), breaks=c(0,0.5,1.5),
+     legend=FALSE, axes=FALSE, box=FALSE)
+plot(paveMap, col=c(NA,paveCol), breaks=c(0,0.5,1.5),
+     legend=FALSE, add=TRUE, axes=FALSE, box=FALSE)
+plot(buildMap, col=c(NA,buildCol), breaks=c(0,0.5,1.5),
+     legend=FALSE, add=TRUE, axes=FALSE, box=FALSE)
+par(mai=c(0,0,0,0))
+plot(origAll, col=gray(1:100/100),
+     legend=FALSE, axes=FALSE, box=FALSE)
+
+
+mapview(treeMap)
+mapview(buildMap)
+
+help("plot")
