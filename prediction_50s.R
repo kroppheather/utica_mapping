eca0690c1ceaffdf8891fe303ef3ecbd24c9eaef
Heather Kropp <kroppheather@gmail.com>
2022-05-27 11:05:51 -0400

    fixed directory error

--- prediction_50s.R
+++ prediction_50s.R
@@ -2,6 +2,9 @@ library(raster)
 library(sf)
 library(mapview)
 
+#########################
+####### Image merge -----
+
 dirP <- "E:/Google Drive/research/projects/utica/model_save/1950/prediction_256/image"
 dirI <- "E:/Google Drive/research/projects/utica/model_save/1950/img_tile256/predict50"
 
@@ -16,7 +19,7 @@ for(i in 1:Nimg){
   
 }
 
-treeAll <- do.call(merge, treeImg,tolerance=0.)
+treeAll <- do.call(merge, treeImg)
 
 
 
@@ -50,20 +53,132 @@ for(i in 1:Nimg){
 origAll <- do.call(merge, origImg)
 
 
+#########################
+####### Offset 2 merge -----
+
+
+dirP2 <- "E:/Google Drive/research/projects/utica/model_save/1950/prediction_256/image_2"
+dirI2 <- "E:/Google Drive/research/projects/utica/model_save/1950/img_tile256/predict50_2"
+
+
+treeImg2 <- list()
+
+Nimg2 <- 435
+
+for(i in 1:Nimg2){
+  treeImg2[[i]] <- raster(paste0(dirP2,"/tree/tree_",i,".tif"))
+  
+  
+}
+
+treeAll2 <- do.call(merge, treeImg2)
+
+
+
+paveImg2 <- list()
+
+for(i in 1:Nimg2){
+  paveImg2[[i]] <- raster(paste0(dirP2,"/pavement/pavement_",i,".tif"))
+  
+  
+}
+
+paveAll2 <- do.call(merge, paveImg2)
+
+
+buildImg2 <- list()
+for(i in 1:Nimg){
+  buildImg2[[i]] <- raster(paste0(dirP2,"/building/building_",i,".tif"))
+  
+  
+}
+
+buildAll2 <- do.call(merge, buildImg2)
+
+origImg2 <- list()
+for(i in 1:Nimg2){
+  origImg2[[i]] <- raster(paste0(dirI2,"/predict_",i,".tif"))
+  
+  
+}
+
+origAll2 <- do.call(merge, origImg2)
+
+
+
+#########################
+####### Offset 3 merge -----
+
+
+dirP3 <- "E:/Google Drive/research/projects/utica/model_save/1950/prediction_256/image_3"
+dirI3 <- "E:/Google Drive/research/projects/utica/model_save/1950/img_tile256/predict50_3"
+
+
+treeImg3 <- list()
+
+Nimg3 <- 392
+
+for(i in 1:Nimg3){
+  treeImg3[[i]] <- raster(paste0(dirP3,"/tree/tree_",i,".tif"))
+  
+  
+}
+
+treeAll3 <- do.call(merge, treeImg3)
+
+
+
+paveImg3 <- list()
+
+for(i in 1:Nimg3){
+  paveImg3[[i]] <- raster(paste0(dirP3,"/pavement/pavement_",i,".tif"))
+  
+  
+}
+
+paveAll3 <- do.call(merge, paveImg3)
+
+
+buildImg3 <- list()
+for(i in 1:Nimg){
+  buildImg3[[i]] <- raster(paste0(dirP3,"/building/building_",i,".tif"))
+  
+  
+}
+
+buildAll3 <- do.call(merge, buildImg3)
+
+origImg3 <- list()
+for(i in 1:Nimg3){
+  origImg3[[i]] <- raster(paste0(dirI3,"/predict_",i,".tif"))
+  
+  
+}
+
+origAll3 <- do.call(merge, origImg3)
+
+
+
+
 # check out weird line artifacts:
 
 plot(treeImg[[300]])
+plot(treeImg2[[300]], add=TRUE)
+
 plot(treeImg[[301]], add=TRUE)
 plot(treeImg[[299]], add=TRUE)
 
 
 plot(origImg[[300]], col=grey(1:100/100))
+plot(origImg2[[300]], col=grey(1:100/100),add=TRUE)
+
+
 plot(origImg[[301]], col=grey(1:100/100),add=TRUE)
 plot(origImg[[299]], col=grey(1:100/100),add=TRUE)
 
 treeMap <- calc(treeAll,function(x){ifelse(x <= 0.3, 0, 1)})
-buildMap <- calc(buildAll,function(x){ifelse(x <= 0.2, 0, 1)})
-paveMap <- calc(paveAll,function(x){ifelse(x <= 0.2, 0, 1)})
+buildMap <- calc(buildAll,function(x){ifelse(x <= 0.15, 0, 1)})
+paveMap <- calc(paveAll,function(x){ifelse(x <= 0.15, 0, 1)})
 
 
 
