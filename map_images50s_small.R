9c7322d3e9081eb6f1312721061dd9ee2502c1c4
kroppheather <kroppheather@gmail.com>
2022-06-29 16:39:27 -0400

    Update map_images50s_small.R
    updated code to include offset and images for predition

--- map_images50s_small.R
+++ map_images50s_small.R
@@ -216,59 +216,70 @@ for(i in 1:nrow(subDF)){
              format="GTiff")
 }
 
-testp <- list()
-rowcountp <- numeric()
-colcountp <- numeric()
-for(i in 1:nrow(subDF)){
-testp[[i]] <- raster(paste0(dirO,"/predict50/predict_",i,".tif"))
-rowcountp[i] <- testp[[i]]@nrows
-colcountp[i] <- testp[[i]]@ncols
-}
+# break up into tiles to do offset:
 
+colsSeq2 <- seq(25,((cols50-1)*128)+25, by=128)
+rowsSeq2 <- seq(25,(rows50*128-1)-25, by=128)
+subDF2 <- data.frame(cols=rep(colsSeq2,times=length(rowsSeq2)),
+                     rows=rep(rowsSeq2,each=length(colsSeq2)))
 
-
-#read in test
-testOut <- raster("/Volumes/GoogleDrive/My Drive/research/projects/utica/50s_valid_out/valid_out.tif")
-
-plot(testOut)
-
-
-treesPredict <- list()
-for(i in 1:20){
-  treesPredict[[i]] <- raster(paste0("/Volumes/GoogleDrive/My Drive/research/projects/utica/50s_valid_out/tree/tree_predict_",i,".tif"))
-  
-}
-
-plot(u50rp, col=gray(1:100/100))
-plot(treesPredict[[1]], add=TRUE)
-plot(treesPredict[[2]], add=TRUE)
-plot(treesPredict[[3]], add=TRUE)
-plot(treesPredict[[4]], add=TRUE)
-plot(treesPredict[[5]], add=TRUE)
-
-mapview(u50rp, col=gray(1:100/100))+
-  mapview(treesPredict[[1]])
-
-
-# break up into tiles
-colsSeq <- seq(1,cols50*128, by=128)
-rowsSeq <- seq(1,rows50*128, by=128)
-subDF <- data.frame(cols=rep(colsSeq,times=length(rowsSeq)),
-                    rows=rep(rowsSeq,each=length(colsSeq)))
 #subdivide raster into 256 x 256
-sub50s <- list()
-rowcount <- numeric()
-colcount <- numeric()
+sub50s2 <- list()
+rowcount2 <- numeric()
+colcount2 <- numeric()
 #this will shave off extra off south and west 
-for(i in 1:nrow(subDF)){
-   sub50s[[i]] <- crop(u50rp, extent(u50rp,  subDF$rows[i], 
-                                     subDF$rows[i]+255,
-                                     subDF$cols[i], 
-                                     subDF$cols[i]+255))
-   rowcount[i] <- sub50s[[i]]@nrows
-   colcount[i] <- sub50s[[i]]@ncols
+for(i in 1:nrow(subDF2)){
+   sub50s2[[i]] <- crop(u50rp, extent(u50rp,  subDF2$rows[i], 
+                                      subDF2$rows[i]+127,
+                                      subDF2$cols[i], 
+                                      subDF2$cols[i]+127))
+   rowcount2[i] <- sub50s2[[i]]@nrows
+   colcount2[i] <- sub50s2[[i]]@ncols
 }
-sub50s[[1]]@ncols
 
-m <- do.call(merge, sub50s)
-plot(m, col=gray(1:100/100))
+sub50s2[[1]]@ncols
+
+m2 <- do.call(merge, sub50s2)
+plot(m2, col=gray(1:100/100))
+#save
+
+for(i in 1:nrow(subDF2)){
+   writeRaster(sub50s2[[i]],
+               paste0("/Volumes/GoogleDrive/My Drive/research/projects/utica/model_save/1950/prediction_128/image_2/predict_",i,".tif"),
+               format="GTiff")
+}
+
+# break up into tiles to do second offset:
+
+colsSeq3 <- seq(100,((cols50-2)*128)+100, by=128)
+rowsSeq3 <- seq(100,((rows50-2)*128)+100, by=128)
+subDF3 <- data.frame(cols=rep(colsSeq3,times=length(rowsSeq3)),
+                     rows=rep(rowsSeq3,each=length(colsSeq3)))
+
+#subdivide raster into 256 x 256
+sub50s3 <- list()
+rowcount3 <- numeric()
+colcount3 <- numeric()
+#this will shave off extra off south and west 
+for(i in 1:nrow(subDF3)){
+   sub50s3[[i]] <- crop(u50rp, extent(u50rp,  subDF3$rows[i], 
+                                      subDF3$rows[i]+127,
+                                      subDF3$cols[i], 
+                                      subDF3$cols[i]+127))
+   rowcount3[i] <- sub50s3[[i]]@nrows
+   colcount3[i] <- sub50s3[[i]]@ncols
+}
+
+plot(sub50s[[1]])
+plot(sub50s2[[1]])
+plot(sub50s3[[1]])
+
+m3 <- do.call(merge, sub50s3)
+plot(m3, col=gray(1:100/100))
+#save
+
+for(i in 1:nrow(subDF3)){
+   writeRaster(sub50s3[[i]],
+               paste0("/Volumes/GoogleDrive/My Drive/research/projects/utica/model_save/1950/prediction_128/image_3/predict_",i,".tif"),
+               format="GTiff")
+}
