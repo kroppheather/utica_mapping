19e94a6adadad9c2c51d8963e84de6d412262f0e
Heather Kropp <kroppheather@gmail.com>
2022-07-06 09:49:31 -0400

    Update prediction_50s.R

--- prediction_50s.R
+++ prediction_50s.R
@@ -282,3 +282,9 @@ plot(uticaRes, breaks=c(-0.1,0.5,#breaks between other
 
 dev.off()
 
+#save raster
+
+writeRaster(uticaRes, "E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_256.tif", format="GTiff" )
+
+test <- raster("E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_256.tif")
+plot(test)
