bc79b943a88f695261d49867e66aa67e7f490474
kroppheather <kroppheather@gmail.com>
2022-07-13 13:50:30 -0400

    img 6
    only trees

--- map_images_17.R
+++ map_images_17.R
@@ -192,7 +192,7 @@ writeRaster(paveMask, paste0(dirM[dirN],"/pavement/pavement_mask_",trainNum,".ti
 
 ###### Validation ----
 
-validNum <- 5
+validNum <- 6
 
 imgN <- stack(paste0(dirMV[dirN], "/images/valid_",validNum,".tif"))
 plotRGB(imgN)
