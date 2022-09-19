6827cfb8470639e52fc110b97cbfc7fdb9a0d246
kroppheather <kroppheather@gmail.com>
2022-09-19 19:49:38 -0400

    img 36

--- map_images50s_strat.R
+++ map_images50s_strat.R
@@ -101,7 +101,7 @@ test <- imgV[1:128,1:128]
 #### Step 1: read in image   ##
 
 #give training image number
-trainNum <- 35
+trainNum <- 36
 
 imgN <- raster(paste0("/Volumes/GoogleDrive/My Drive/research/projects/utica/model_save/1950/data_strat/image/train_",trainNum,".tif"))
 plot(imgN, col=grey(1:100/100))
