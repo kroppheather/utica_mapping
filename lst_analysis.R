47c15670f8833e42f9a44ae53cc4516d85c2664c
Heather Kropp <kroppheather@gmail.com>
2022-09-28 12:24:26 -0400

    Update lst_analysis.R

--- lst_analysis.R
+++ lst_analysis.R
@@ -156,9 +156,10 @@ stcA
 source("c:/Users/hkropp/Documents/census_key.r")
 census_api_key(key)
 
-#total population, 2 white alone, 3, black alone 
+#total population, 2 white alone, 3, black alone , 4= native american alone, 5 = asian alone, 8 = two or more races
 race <- get_acs("tract", state="36", county="065",
-                variables = c("B02001_001","B02001_002", "B02001_003"),year=2020, geometry=TRUE)
+                variables = c("B02001_001","B02001_002", "B02001_003", "B02001_004", "B02001_005", "B02001_008"),
+                year=2020, geometry=TRUE, output="wide")
 #
 # median household income in dollars
 income_house <- get_acs("tract", state="36", county="065",
