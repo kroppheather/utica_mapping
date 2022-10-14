81b390dad5bb64024a3d8e1ae072aca8c5db6aa8
Heather Kropp <kroppheather@gmail.com>
2022-10-14 15:12:59 -0400

    maps and stratified images 17

--- comparision_17s.R
+++ comparision_17s.R
@@ -38,7 +38,7 @@ treeCol1 <- rgb(0.13,0.54,0.13)
 paveCol1 <- "grey30"
 buildCol1 <- "tomato4"
 
-png("E:/Google Drive/research/projects/utica/model_save/2017/all_maps/maps/comp_17.png", height=15616*2,width=33280*2)
+png("E:/Google Drive/research/projects/utica/model_save/2017/all_maps/maps/comp_17.png", height=15616,width=33280)
 
 par(mfrow=c(1,2))
 
@@ -47,7 +47,7 @@ plot(map17_256, axes=FALSE, legend=FALSE, box=FALSE,
               1.5, # tree
               2.5, # building
               3.5), #pavement
-     maxpixels=7424*3840 ,
+     maxpixels=33280*15616 ,
      col=c(NA, treeCol1,buildCol1, paveCol1))
 mtext("Training n=60: 256 x 256", side=3, cex=20, line=-50)
 
@@ -57,7 +57,7 @@ plot(map17_128, axes=FALSE, legend=FALSE, box=FALSE,
               2.5, # building
               3.5 ), #pavement
      col=c(NA, treeCol1,buildCol1, paveCol1),
-     maxpixels=7424*3840
+     maxpixels=33280*15616
      )
 mtext("Training n=120: 128 x 128", side=3, cex=20, line=-30)
 
@@ -135,89 +135,6 @@ pave_128 <- reclassify(map17_128, matrix(c(0,0,
                                            1,0,
                                            2,0,
                                            3,1), ncol=2, byrow=TRUE))
-
-
-
-
-##### IOU calc kern ----
-treesCrop_kern <- list()
-treesResamp_kern <- list()
-buildCrop_kern <- list()
-buildResamp_kern <- list()
-paveCrop_kern <- list()
-paveResamp_kern <- list()
-
-for(i in 1:nValid){
-  treesCrop_kern[[i]] <- crop(trees_kern, treesMask[[i]])
-  treesResamp_kern[[i]] <- resample(treesCrop_kern[[i]], treesMask[[i]], method="ngb")
-  buildCrop_kern[[i]] <- crop(build_kern, buildMask[[i]])
-  buildResamp_kern[[i]] <- resample(buildCrop_kern[[i]], buildMask[[i]], method="ngb")
-  paveCrop_kern[[i]] <- crop(pave_kern, paveMask[[i]])
-  paveResamp_kern[[i]] <- resample(paveCrop_kern[[i]], paveMask[[i]], method="ngb")
-  
-}
-
-
-
-
-
-
-treeOverlap_kern <- list()
-buildOverlap_kern <- list()
-paveOverlap_kern <- list()
-
-for(i in 1:nValid){
-  treeOverlap_kern[[i]] <- treesResamp_kern[[i]] + treesMask[[i]]
-  buildOverlap_kern[[i]] <- buildResamp_kern[[i]] + buildMask[[i]]
-  paveOverlap_kern[[i]] <- paveResamp_kern[[i]] + paveMask[[i]]
- 
-}
-#IOU
-treeCalc_kern <- list()
-buildCalc_kern <- list()
-paveCalc_kern <- list()
-for(i in 1:nValid){
-  treeCalc_kern[[i]] <- freq(treeOverlap_kern[[i]])
-  buildCalc_kern[[i]] <- freq(buildOverlap_kern[[i]])
-  paveCalc_kern[[i]] <- freq(paveOverlap_kern[[i]])
-  
-}
-
-
-
-treeSum_kern <- do.call("rbind", treeCalc_kern)
-colnames(treeSum_kern) <- c("overlapID","pix")
-treeSum_kern <- data.frame(treeSum_kern)
-
-buildSum_kern <- do.call("rbind", buildCalc_kern)
-colnames(buildSum_kern) <- c("overlapID","pix")
-buildSum_kern <- data.frame(buildSum_kern)
-
-paveSum_kern <- do.call("rbind", paveCalc_kern)
-colnames(paveSum_kern) <- c("overlapID","pix")
-paveSum_kern <- data.frame(paveSum_kern)
-
-# some masks on edge of offset image produced that changed original extent
-# need to remove NA
-
-
-totalTreePix_kern <- na.omit(treeSum_kern) %>%
-  group_by(overlapID) %>%
-  summarize(totalPix = sum(pix))
-
-totalBuildPix_kern <- na.omit(buildSum_kern) %>%
-  group_by(overlapID) %>%
-  summarize(totalPix = sum(pix))
-
-totalPavePix_kern <- na.omit(paveSum_kern) %>%
-  group_by(overlapID) %>%
-  summarize(totalPix = sum(pix))
-
-
-
-IOU_tree_kern <- totalTreePix_kern[3,2]/(totalTreePix_kern[2,2]+totalTreePix_kern[3,2])
-IOU_build_kern <- totalBuildPix_kern[3,2]/(totalBuildPix_kern[2,2]+totalBuildPix_kern[3,2])
-IOU_pave_kern <- totalPavePix_kern[3,2]/(totalPavePix_kern[2,2]+totalPavePix_kern[3,2])
 
 
 
@@ -394,14 +311,14 @@ IOU_pave_128 <- totalPavePix_128[3,2]/(totalPavePix_128[2,2]+totalPavePix_128[3,
 ## 256 ##
 
 #0 is other, 1 = tree, 2= building, 3 = pavement
-treeEx_256 <- na.omit(data.frame(prediction = extract(map50_256, treeValid),
+treeEx_256 <- na.omit(data.frame(prediction = extract(map17_256, treeValid),
                          actual = rep(1, nrow(treeValid))))
 
-buildEx_256 <- na.omit(data.frame(prediction = extract(map50_256, buildValid),
+buildEx_256 <- na.omit(data.frame(prediction = extract(map17_256, buildValid),
                                  actual = rep(2, nrow(buildValid))))
-paveEx_256 <- na.omit(data.frame(prediction = extract(map50_256, paveValid),
+paveEx_256 <- na.omit(data.frame(prediction = extract(map17_256, paveValid),
                                  actual = rep(3, nrow(paveValid))))
-otherEx_256 <- na.omit(data.frame(prediction = extract(map50_256, otherValid),
+otherEx_256 <- na.omit(data.frame(prediction = extract(map17_256, otherValid),
                                  actual = rep(0, nrow(otherValid))))
 
 data_comp_256 <- rbind(treeEx_256, buildEx_256, paveEx_256, otherEx_256)
@@ -425,49 +342,17 @@ build_PA_256 <-  conf_256$table[4,4]/sum(conf_256$table[4,])
 
 
 
-## kernal ##
-
-#0 is other, 1 = tree, 2= building, 3 = pavement
-treeEx_kern <- na.omit(data.frame(prediction = extract(map50_kern, treeValid),
-                                 actual = rep(1, nrow(treeValid))))
-
-buildEx_kern <- na.omit(data.frame(prediction = extract(map50_kern, buildValid),
-                                  actual = rep(2, nrow(buildValid))))
-paveEx_kern <- na.omit(data.frame(prediction = extract(map50_kern, paveValid),
-                                 actual = rep(3, nrow(paveValid))))
-otherEx_kern <- na.omit(data.frame(prediction = extract(map50_kern, otherValid),
-                                  actual = rep(0, nrow(otherValid))))
-
-data_comp_kern <- rbind(treeEx_kern, buildEx_kern, paveEx_kern, otherEx_kern)
-
-conf_kern <- confusionMatrix(as.factor(data_comp_kern$prediction), as.factor(data_comp_kern$actual))
-
-
-conf_kern$table
-
-conf_kern$overall[1]
-
-other_UA_kern <- conf_kern$table[1,1]/sum(conf_kern$table[,1])
-tree_UA_kern <-  conf_kern$table[2,2]/sum(conf_kern$table[,2])
-pave_UA_kern <-  conf_kern$table[3,3]/sum(conf_kern$table[,3])
-build_UA_kern <-  conf_kern$table[4,4]/sum(conf_kern$table[,4])
-
-other_PA_kern <- conf_kern$table[1,1]/sum(conf_kern$table[1,])
-tree_PA_kern <-  conf_kern$table[2,2]/sum(conf_kern$table[2,])
-pave_PA_kern <-  conf_kern$table[3,3]/sum(conf_kern$table[3,])
-build_PA_kern <-  conf_kern$table[4,4]/sum(conf_kern$table[4,])
-
 ## 128 ##
 
 #0 is other, 1 = tree, 2= building, 3 = pavement
-treeEx_128 <- na.omit(data.frame(prediction = extract(map50_128, treeValid),
+treeEx_128 <- na.omit(data.frame(prediction = extract(map17_128, treeValid),
                                  actual = rep(1, nrow(treeValid))))
 
-buildEx_128 <- na.omit(data.frame(prediction = extract(map50_128, buildValid),
+buildEx_128 <- na.omit(data.frame(prediction = extract(map17_128, buildValid),
                                   actual = rep(2, nrow(buildValid))))
-paveEx_128 <- na.omit(data.frame(prediction = extract(map50_128, paveValid),
+paveEx_128 <- na.omit(data.frame(prediction = extract(map17_128, paveValid),
                                  actual = rep(3, nrow(paveValid))))
-otherEx_128 <- na.omit(data.frame(prediction = extract(map50_128, otherValid),
+otherEx_128 <- na.omit(data.frame(prediction = extract(map17_128, otherValid),
                                   actual = rep(0, nrow(otherValid))))
 
 data_comp_128 <- rbind(treeEx_128, buildEx_128, paveEx_128, otherEx_128)
@@ -491,21 +376,21 @@ build_PA_128 <-  conf_128$table[4,4]/sum(conf_128$table[4,])
 
 
 #output table
-MetOut <- data.frame(class=rep(c("tree", "building","pavement"), each=3),
-                     model=rep(c("256","128","kernal"), times=3),
-                     users.Accuracy=c(tree_UA_256,tree_UA_128,tree_UA_kern,
-                                      build_UA_256,build_UA_128,build_UA_kern,
-                                      pave_UA_256,pave_UA_128,pave_UA_kern),
-                     producers.Accuracy = c(tree_PA_256,tree_PA_128,tree_PA_kern,
-                                            build_PA_256,build_PA_128,build_PA_kern,
-                                            pave_PA_256,pave_PA_128,pave_PA_kern),
-                      IOU=c(IOU_tree_256$totalPix, IOU_tree_128$totalPix,IOU_tree_kern$totalPix,
-                           IOU_build_256$totalPix,IOU_build_128$totalPix,IOU_build_kern$totalPix, 
-                           IOU_pave_256$totalPix,IOU_pave_128$totalPix, IOU_pave_kern$totalPix),
-                     total.Accuracy=(rep(c(conf_256$overall[1], conf_128$overall[1],conf_kern$overall[1]),times=3)))
+MetOut <- data.frame(class=rep(c("tree", "building","pavement"), each=2),
+                     model=rep(c("256","128"), times=3),
+                     users.Accuracy=c(tree_UA_256,tree_UA_128,
+                                      build_UA_256,build_UA_128,
+                                      pave_UA_256,pave_UA_128),
+                     producers.Accuracy = c(tree_PA_256,tree_PA_128,
+                                            build_PA_256,build_PA_128,
+                                            pave_PA_256,pave_PA_128),
+                      IOU=c(IOU_tree_256$totalPix, IOU_tree_128$totalPix,
+                           IOU_build_256$totalPix,IOU_build_128$totalPix, 
+                           IOU_pave_256$totalPix,IOU_pave_128$totalPix),
+                     total.Accuracy=(rep(c(conf_256$overall[1], conf_128$overall[1]),times=3)))
 
 
-write.table(MetOut, "E:/Google Drive/research/projects/utica/model_save/1950/all_maps/metric_comp.csv",
+write.table(MetOut, "E:/Google Drive/research/projects/utica/model_save/2017/all_maps/metric_comp.csv",
             sep=",", row.names=FALSE)
 
 
