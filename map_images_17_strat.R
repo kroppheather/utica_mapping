6503cfefcef1fddf72ecc6929ed380aae2c362a7
Heather Kropp <kroppheather@gmail.com>
2022-10-25 20:51:11 -0400

    img 13

--- map_images_17_strat.R
+++ map_images_17_strat.R
@@ -179,7 +179,7 @@ samplesy2 <- sample(1:(u17a@nrows-257), nSamp2)[81]
 #### Step 1: read in image   ##
 
 #give training image number
-trainNum <- 12
+trainNum <- 13
 
 imgN <- stack(paste0(dirO[dirN], "/train_",trainNum,".tif"))
 plotRGB(imgN)
