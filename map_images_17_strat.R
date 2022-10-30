65b4d5b0c96b060350dad0ac52133430b89a2e0a
Heather Kropp <kroppheather@gmail.com>
2022-10-30 12:57:08 -0400

    img 28
    tree only

--- map_images_17_strat.R
+++ map_images_17_strat.R
@@ -179,7 +179,7 @@ samplesy2 <- sample(1:(u17a@nrows-257), nSamp2)[81]
 #### Step 1: read in image   ##
 
 #give training image number
-trainNum <- 27
+trainNum <- 28
 
 imgN <- stack(paste0(dirO[dirN], "/train_",trainNum,".tif"))
 plotRGB(imgN)
