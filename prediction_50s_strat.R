7303adbf3d040240980d9bf4c688076284a65dbb
Heather Kropp <kroppheather@gmail.com>
2022-10-30 11:35:16 -0400

    img 21
    organized script and saving 50 strat

--- prediction_50s_strat.R
+++ prediction_50s_strat.R
@@ -271,4 +271,4 @@ plot(uticaRes, breaks=c(-0.1,0.5,#breaks between other
 
 dev.off()
 
-writeRaster(uticaRes, "E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_128.tif", format="GTiff" )
+writeRaster(uticaRes, "E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_strat.tif", format="GTiff" )
