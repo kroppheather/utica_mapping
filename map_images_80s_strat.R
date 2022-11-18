7259dc4ec2b31a1232c1ba3b5b5f17dbe76332ff
Heather Kropp <kroppheather@gmail.com>
2022-11-18 12:11:02 -0500

    img 37

--- map_images_80s_strat.R
+++ map_images_80s_strat.R
@@ -115,7 +115,7 @@ dfSubA <- do.call( "rbind", dfSub)
 #### Step 1: read in image   ##
 
 #give training image number
-trainNum <- 36
+trainNum <- 37
 
 imgN <- raster(paste0(dirO, "/train_",trainNum,".tif"))
 plot(imgN, col=grey(1:100/100))
