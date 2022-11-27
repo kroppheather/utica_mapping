6953a9be56c6b31bb9599988f59f4c27bcde6097
Heather Kropp <kroppheather@gmail.com>
2022-11-27 14:53:41 -0500

    Update agu_figures_analysis.R

--- agu_figures_analysis.R
+++ agu_figures_analysis.R
@@ -3,9 +3,10 @@
 
 library(raster)
 
-lc1957s <- raster("E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_strat.tif")
+lc1957s <- raster("E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_strat2.tif")
 lc1957 <- raster("E:/Google Drive/research/projects/utica/model_save/1950/all_maps/utica50s_128.tif")
 lc2017 <-  raster("E:/Google Drive/research/projects/utica/model_save/2017/all_maps/utica17_256.tif")
+lc2017s <-  raster("E:/Google Drive/research/projects/utica/model_save/2017/all_maps/utica17_strat.tif")
 plot(lc1957)
 plot(lc2017)
 
@@ -30,12 +31,18 @@ trees17 <- reclassify(lc2017, rcl=matrix(c(0,NA,
                                          2,NA,
                                          3,NA), ncol=2, byrow=TRUE))
 
+trees17s <- reclassify(lc2017s, rcl=matrix(c(0,NA,
+                                           1,1,
+                                           2,NA,
+                                           3,NA), ncol=2, byrow=TRUE))
+
 plot(trees17)
 
 
-writeRaster(trees57s, "E:/Google Drive/research/projects/utica/model_save/1950/all_maps/Utrees57_strat.tif",
+# writeRaster(trees57s, "E:/Google Drive/research/projects/utica/model_save/1950/all_maps/Utrees57_strat2.tif",
             format="GTiff")
 
 writeRaster(trees17, "E:/Google Drive/research/projects/utica/model_save/2017/all_maps/Utrees17_256.tif",
             format="GTiff")
-
+writeRaster(trees17s, "E:/Google Drive/research/projects/utica/model_save/2017/all_maps/Utrees17_strat.tif",
+            format="GTiff")
