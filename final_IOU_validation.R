cb58bf7da136442cb179b2fdb3e360f63638f894
Heather Kropp <kroppheather@gmail.com>
2022-11-28 21:26:27 -0500

    img 15

--- final_IOU_validation.R
+++ final_IOU_validation.R
@@ -53,7 +53,7 @@ dirM <- "E:/Google Drive/research/projects/utica/model_save/1950/IOU_valid/masks
 
 
 #give training image number
-trainNum <- 14
+trainNum <- 15
 
 imgN <- raster(paste0(dirO[dirN], "/valid_",trainNum,".tif"))
 plot(imgN, col=gray(1:100/100))
