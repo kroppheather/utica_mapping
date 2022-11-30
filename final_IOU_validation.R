2826c419a627a6e9d53e9d0e38bd1f9d51df70f2
Heather Kropp <kroppheather@gmail.com>
2022-11-29 20:22:27 -0500

    img 15

--- final_IOU_validation.R
+++ final_IOU_validation.R
@@ -155,7 +155,7 @@ dirM <- "E:/Google Drive/research/projects/utica/model_save/2017/IOU_valid/masks
 
 
 #give training image number
-trainNum <- 14
+trainNum <- 15
 
 imgN <- stack(paste0(dirO[dirN], "/valid_",trainNum,".tif"))
 plotRGB(imgN)
