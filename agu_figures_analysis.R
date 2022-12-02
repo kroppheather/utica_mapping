a11ec976c313cdf7e26c2fa6a5d3c43d0f2e7ad3
Heather Kropp <kroppheather@gmail.com>
2022-12-02 16:43:26 -0500

    Update agu_figures_analysis.R

--- agu_figures_analysis.R
+++ agu_figures_analysis.R
@@ -2,3 +2,37 @@ library(raster)
 library(sf)
 library(dplyr)
 
+
+# read in land cover
+
+
+
+# read in temperature
+
+
+# read in census
+
+
+
+# land cover viz
+
+# land cover change
+
+
+
+# patterns with census data
+
+# land cover change by tract
+
+# 2017 land cover by tract
+
+# temperature vs 2017 tree cover
+
+# 2017 tree cover vs income
+
+# 2017 tree cover vs renting
+
+# 2017 tree cover vs race
+
+# land cover change patterns
+
