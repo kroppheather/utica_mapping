42becc0115309b7bf1508ea026d98511b6613185
Heather Kropp <kroppheather@gmail.com>
2023-03-17 12:44:27 -0400

    Update Analysis_figures.R

--- Analysis_figures.R
+++ Analysis_figures.R
@@ -1,1 +1,9 @@
 #reads in data created by final maps
+
+#in final maps:
+# 1957: utica stratified sampling (fixed from overlay error in prediction file), 128 x128
+# 1987: utica stratified sampling 128 x 128
+# 1957: utica stratified sampling 256 x 256
+# census shape files from the ACS 2020
+# average summer land surface temperature from landsat collection 2 level 2
+
